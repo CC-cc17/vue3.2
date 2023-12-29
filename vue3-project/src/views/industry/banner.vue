5424f37e0a610906f2ba261b3d77736ede98bbbb
Skadi <2983402895@qq.com>
2023-12-29 22:55:18 +0800

    完成行业信息页面

--- vue3-project/src/views/industry/banner.vue
+++ vue3-project/src/views/industry/banner.vue
@@ -6,17 +6,16 @@ import {ref} from 'vue'
 <template>
     <div class="industry-banner">
       <div class="content">
-        <h1>行业标题</h1>
+        <h1>行业资讯</h1>
         <div class="description">
           <div class="text">
-            <p>这是第一个段落的内容。</p>
-            <p>这是第二个段落的内容。</p>
+            <p>探索各行各业,行行出状元！</p>
+            <p>阅读行业概况,深入了解每个行业的情况,</p>
+            <p>计划未来职业和发展前景。</p><br>
+            <p>以下20个行业来自中华人民共和国国家标准国民经济行业分类(GB/T 4754—2017)</p>
+            <p>由中华人民共和国国家质量监督检验检疫总局及中国国家标准化管理委员会发布。</p>
           </div>
-          <img src="@/assets/images/logo.png" alt="描述性图片">
-        </div>
-        <div class="additional-info">
-          <p>这里是一些额外的信息。</p>
-          <img src="@/assets/images/logo.png" alt="装饰性图片">
+          <img class="image-container" src="@/assets/images/ind_ban_logo.png" alt="三百六十行,行行出状元">
         </div>
       </div>
     </div>
@@ -25,8 +24,8 @@ import {ref} from 'vue'
   <style scoped>
   .industry-banner {
     width: 100%;
-    height: 450px;
-    background-color: #f5f5f5; /* Adjust the background color as needed */
+    height: 500px;
+    background-color: white; /* Adjust the background color as needed */
     display: flex;
     justify-content: center;
     align-items: center;
@@ -40,34 +39,38 @@ import {ref} from 'vue'
   .industry-banner .description {
     display: flex;
     align-items: center;
-    margin-bottom: 30px; /* Adjust as needed */
+    margin-bottom: 30px; 
   }
   
   .industry-banner .description .text {
-    margin-right: 20px; /* Adjust the space between the text and the image */
+    margin-right: 20px; 
+    font-size:18px;
   }
   
   .industry-banner .description img {
-    width: 100px; /* Adjust as needed */
+    width: 400px; 
     height: auto;
   }
   
   .industry-banner .additional-info {
-    position: absolute;
+    position: relative;
     left: 50%;
     transform: translateX(-50%);
-    width: calc(100% / 3);
+    width: 800px;
     background-color: #2462a8;
     color: white;
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 100px; /* Adjust as needed */
+    height: 75px;
+    font-size: 30px;
+    border-radius: 30px;
   }
-  
-  .industry-banner .additional-info img {
-    width: 50px; /* Adjust as needed */
-    height: auto;
-    margin-left: 20px; /* Adjust the space between the text and the image */
-  }
+.image-container {
+  transition: transform 0.4s ease; 
+}
+
+.image-container:hover {
+  transform: scale(1.4); 
+}
   </style>
--- vue3-project/src/views/industry/cards.vue
+++ vue3-project/src/views/industry/cards.vue
@@ -1,14 +1,88 @@
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
+
+// 行业信息,图片没压缩
+
+const industries = ref([
+    { id: 1, title: '农、林、牧、渔业', imagePath: 'src/assets/images/industries/1.jpg', route: '/industry/agriculture' },
+    { id: 2, title: '采矿业', imagePath: 'src/assets/images/industries/2.jpg', route: '/industry/mining' },
+    { id: 3, title: '制造业', imagePath: 'src/assets/images/industries/3.jpg', route: '/industry/mining' },
+    { id: 4, title: '电力、热力、燃气及水生产和供应业', imagePath: 'src/assets/images/industries/4.jpg', route: '/industry/mining' },
+    { id: 5, title: '建筑业', imagePath: 'src/assets/images/industries/5.jpg', route: '/industry/mining' },
+    { id: 6, title: '批发和零售业', imagePath: 'src/assets/images/industries/6.jpg', route: '/industry/mining' },
+    { id: 7, title: '交通运输、仓储和邮政业', imagePath: 'src/assets/images/industries/7.jpg', route: '/industry/mining' },
+    { id: 8, title: '住宿和餐饮业', imagePath: 'src/assets/images/industries/8.jpg', route: '/industry/mining' },
+    { id: 9, title: '信息传输、软件和信息技术服务业', imagePath: 'src/assets/images/industries/9.jpg', route: '/industry/mining' },
+    { id: 10, title: '金融业', imagePath: 'src/assets/images/industries/10.jpg', route: '/industry/mining' },
+    { id: 11, title: '房地产业', imagePath: 'src/assets/images/industries/11.jpg', route: '/industry/mining' },
+    { id: 12, title: '租赁和商务服务业', imagePath: 'src/assets/images/industries/12.jpg', route: '/industry/mining' },
+    { id: 13, title: '科学研究和技术服务业', imagePath: 'src/assets/images/industries/13.jpg', route: '/industry/mining' },
+    { id: 14, title: '水利、环境和公共设施管理业', imagePath: 'src/assets/images/industries/14.jpg', route: '/industry/mining' },
+    { id: 15, title: '居民服务、修理和其他服务业', imagePath: 'src/assets/images/industries/15.jpg', route: '/industry/mining' },
+    { id: 16, title: '教育', imagePath: 'src/assets/images/industries/16.jpg', route: '/industry/mining' },
+    { id: 17, title: '卫生和社会工作', imagePath: 'src/assets/images/industries/17.jpg', route: '/industry/mining' },
+    { id: 18, title: '文化、体育和娱乐业', imagePath: 'src/assets/images/industries/18.jpg', route: '/industry/mining' },
+    { id: 19, title: '公共管理、社会保障和社会组织', imagePath: 'src/assets/images/industries/19.jpg', route: '/industry/mining' },
+    { id: 20, title: '国际组织', imagePath: 'src/assets/images/industries/20.jpg', route: '/industry/mining' },
+]);
 
 </script>
 
 <template>
     <div>
-        cards
+        <div class="card">
+            <div class="cards-container">
+                <h3>请从下面选择您有兴趣的行业</h3>
+                <el-row :gutter="40">
+                    <el-col v-for="industry in industries" :key="industry.id" :span="8" class="industry-col"
+                        :style="{ marginBottom: '40px' }">
+                        <router-link :to="industry.route">
+                            <el-card shadow="hover" :body-style="{ padding: '0px' }">
+                                <img :src="industry.imagePath" class="image" alt="Industry Image" />
+                                <div style="padding: 14px">
+                                    <span class="card-title">{{ industry.title }}</span>
+                                </div>
+                            </el-card>
+                        </router-link>
+                    </el-col>
+                </el-row>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
+.card{
+    padding: 50px;
+    margin-bottom: 200px;
+}
+.cards-container {
+    padding: 20px;
+}
 
-</style>
+.image {
+    height: 200px;
+    width: 100%;
+    display: block;
+    border-radius: 5px;
+    object-fit: cover
+}
+
+.card-content {
+    padding: 10px;
+}
+
+.el-card {
+    cursor: pointer;
+    transition: transform 0.3s ease;
+}
+
+.el-card:hover {
+    transform: translateY(-5px);
+}
+
+.card-title {
+    font-weight: bold;
+    font-size: 20px;
+    text-decoration: none;
+}</style>
